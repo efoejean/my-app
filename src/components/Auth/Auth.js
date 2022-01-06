d803cb8722b3a17e2dc8ecb8bc291b0e606afb0a
efoejean <efoejean@gmail.com>
2022-01-05 21:37:37 -0600

    Import Input from input

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -3,13 +3,23 @@ import React from 'react'
 import { Avatar,Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'; // npm install @material-ui/icons sepeartely
 import useStyles from './styles';
+import { TextFields } from '@material-ui/icons';
+import Input from './Input';
 
  const Auth = () => {
+    
 
     const classes = useStyles();
 
      const isSignup = false;
 
+     const handleSubmit = (e) => {
+
+     };
+
+     const handleChange = (e) => {
+
+     };
     return (
         <Container component="main" maxWidth="xg">
         <Paper className={classes.paper} elevation={3}>
@@ -17,9 +27,20 @@ import useStyles from './styles';
             <LockOutlinedIcon />
           
         </Avatar>
-        <Typography component="h1" variant="h5">
-        Sign In
-        </Typography>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+            {isSignup && (
+                <>
+                  <Input name="name" label="Name" autoFocus={true} type="text" handleChange={handleChange}/>
+                  <Input name="email" label="Email" autoFocus={false} type="email" handleChange={handleChange}/>
+                  
+                </>
+            )}
+            <Input name="email" label="Email" autoFocus={true} type="email" handleChange={handleChange}/>
+            <Input password="password" label="password" handleChange={handleChange} type="password" autoFocus xs={12} />
+            </Grid>
+        </form>
         </Paper>
 
         </Container>
