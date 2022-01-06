5fc81f7a7f9e00315efe3d53a836626b75f52881
efoejean <efoejean@gmail.com>
2022-01-05 19:07:43 -0600

    Create a Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Grow, Grid } from "@material-ui/core";
+
+export const Home = () => {
+    return (
+        <Grow in>
+            <Container maxWidth="lg">
+                <Grid container spacing={3}>
+            
+                </Grid>
+            </Container>
+        </Grow>
+    )
+}
+
+export default Home;
