07e31aa77705496679608182c6ce8bd93cf091ad
efoejean <efoejean@gmail.com>
2022-01-05 18:39:32 -0600

    using material-ui to styles the AppBar, import it from styles.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { AppBar, Typography } from "@material-ui/core"; 
-import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom"; // 
 
-import React from 'react'
 
-export const Navbar = () => {
-  <AppBar position="static" color="inherit">
-  <Typography variant="h6" color="inherit" align="center">Memoir
-  </Typography>
-  </AppBar>
-}
+import useStyles from "./styles";
+
+ const Navbar = () => {
+
+const classes = useStyles();
+
+    return(
+        // using material-ui to styles the AppBar, import it from styles.js
+        <AppBar className={classes.appbar} position="static" color="inherit"> 
+        <div className={classes.brandContainer}>
+        <Typography  className={classes.heading} variant="h2" align="center">
+          Welcome to React 
+        </Typography>
+        </div>
+        <toolbar className={classes.toolbar}>
+
+        </toolbar>
+    </AppBar>
+    );
+  
+};
 
 export default Navbar;
