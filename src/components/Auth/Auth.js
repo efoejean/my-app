026b77b08ac32677ae081d7ed153b6c1efcea415
efoejean <efoejean@gmail.com>
2022-01-05 20:48:46 -0600

    @material-ui/icons sepeartely,

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
+import { Avatar,Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'; // npm install @material-ui/icons sepeartely
+import useStyles from './styles';
+
  const Auth = () => {
-     const state = null;
+
+    const classes = useStyles();
+
+     const isSignup = false;
 
     return (
-        <div>
-            Auth
-        </div>
+        <Container component="main" maxWidth="xg">
+        <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          
+        </Avatar>
+        <Typography component="h1" variant="h5">
+        Sign In
+        </Typography>
+        </Paper>
+
+        </Container>
     );
 };
 
