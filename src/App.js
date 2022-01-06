f8d4c90cf72f12a0fd062f3b620e421e80348dc5
efoejean <efoejean@gmail.com>
2022-01-05 19:42:34 -0600

    import from React, Create route with the Auth and home compnent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Switch is not working this version. we are using Routes instead of Switch.
+
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Swich, Route } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 
-const App = () => {
-  return (
+const App = () => 
+   (
+
+    <BrowserRouter>
   <Container maxWidth="lg">
+    
     <Navbar/>
-    <Home/>
+    <Routes>
+      <Route exact path="/" exact component={Home} />
+      <Route path="/auth" exact component={Auth} />
+    </Routes>
 
   </Container>
 
-
+</BrowserRouter>
 
   );
-};
+
 
 export default App;
